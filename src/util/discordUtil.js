ff52a93e3bca2f57c276a5804086166261a028bb
SerAM90 <serranoam90@gmail.com>
2025-05-22 19:06:11 -0400

    Added Genre function
    Added the 'genre' category, if there is multiple comments it sorts them by Genre.count, takes the highest two, and then displays those Genre.tags (tags in this instance is the genre name)

--- src/util/discordUtil.js
+++ src/util/discordUtil.js
@@ -10,9 +10,8 @@ const { blockQuote, bold, italic, quote, spoiler, strikethrough, underline, subt
 const sendChannelNewContent = (payload) => {
 	var event = new PlexWebhookPayload(payload)
 	const channel = discordClient.channels.cache.get(channel_id)
-	//channel.send(bold(event.Metadata.title + " " + "(" + event.Metadata.year + ")"))
 	channel.send(displayTitleYearString(event.Metadata.title, event.Metadata.year) + " " +
-			//(displayRatingsString(event.Metadata.audienceRating)) + "\n" + 
+			(displayGenresString(event.Metadata.Genre)) + "\n" + 
 			(displayRatingsString(event.Metadata.Rating)) + "\n" + 
 			(displaySummaryString(event.Metadata.summary))
 )
@@ -25,36 +24,41 @@ function displayTitleYearString(title, year){
 }
 
 function displaySummaryString(summary){
-	return blockQuote(summary) //Having "Summary" + summary felt unnecessary. Just sending summary
+	return blockQuote(summary) 
 }
 
 
 function capitolFirstLetter(Rating){
-	return capitalizedRating = Rating.type.charAt(0).toUpperCase() + Rating.type.slice(1) //CAPITALIZE FIRST LETTER
+	return capitalizedRating = Rating.type.charAt(0).toUpperCase() + Rating.type.slice(1) //Capitalize the first letter of the rating type
 }
 
 function displayRatingsString(Rating){ //CAP "R" for Rating
-	var ratingsString = " " //Empty string
+	var ratingsString = " " 
 	Rating.forEach(Rating => {
-			const ratingsSource = Rating.image.match(/^([a-z]+):\/\//i)?.[1]; //has to be in the loop
-
-		ratingsString += ratingsSource + ": " + Rating.value + " " +
-		 "(" + capitolFirstLetter(Rating) + ")" +", "
+			const ratingsSource = Rating.image.match(/^([a-z]+):\/\//i)?.[1]; 
+		ratingsString += "• " + ratingsSource + ": " + Rating.value + " " +
+		 "(" + capitolFirstLetter(Rating) + ")" +" "
 	});
 
 	return underline(ratingsString) 
 }
 
-function displayGenresString(genre){
-	var genresString = " " //Empty string
-	genre.forEach(Genre => {
-
-		genresString += Genre.count + " " + Genre.tag + " "
-	});
-
-	return italic(genresString) 
-
+function displayGenresString(genre) {
+	if (!Array.isArray(genre) || genre.length === 0) {
+		return italic("Genre not provided");
 	}
+
+	// Sort by Genre.count descending and then take the top 2
+	const top2Genres = genre
+		.sort((FirstHighestCount, SecondHighestCount) => SecondHighestCount.count - FirstHighestCount.count)
+		.slice(0, 2);
+
+	// Build string of just the tags
+	let genresString = top2Genres.map(g => g.tag).join("/");
+
+	return italic(genresString);
+}
+
 
 module.exports = {
 	sendChannelNewContent
