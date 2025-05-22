d3f707b0a9a5b9f12cd9e8e0b72627b6bb1df406
SerAM90 <serranoam90@gmail.com>
2025-05-22 17:47:42 -0400

    Added multiple ratings support, made code more modular - Genre WIP
    Modularized some of the code and broke it up more instead of large functions. Also added support to show multiple ratings, the type of rating (critic or audience), and where the ratings originated from. Implemented more styling to output sent to Discord so it is easier to read.
    Next to work on is 'genre'

--- src/util/discordUtil.js
+++ src/util/discordUtil.js
@@ -10,12 +10,51 @@ const { blockQuote, bold, italic, quote, spoiler, strikethrough, underline, subt
 const sendChannelNewContent = (payload) => {
 	var event = new PlexWebhookPayload(payload)
 	const channel = discordClient.channels.cache.get(channel_id)
-	channel.send(bold(event.Metadata.title + " " + "(" + event.Metadata.year + ")"))
-	channel.send(underline("Rating: " + event.Metadata.audienceRating))	
+	//channel.send(bold(event.Metadata.title + " " + "(" + event.Metadata.year + ")"))
+	channel.send(displayTitleYearString(event.Metadata.title, event.Metadata.year) + " " +
+			//(displayRatingsString(event.Metadata.audienceRating)) + "\n" + 
+			(displayRatingsString(event.Metadata.Rating)) + "\n" + 
+			(displaySummaryString(event.Metadata.summary))
+)
 
 }
 
+function displayTitleYearString(title, year){
+	return bold(title + " " + "(" + year + ")")
 
+}
+
+function displaySummaryString(summary){
+	return blockQuote(summary) //Having "Summary" + summary felt unnecessary. Just sending summary
+}
+
+
+function capitolFirstLetter(Rating){
+	return capitalizedRating = Rating.type.charAt(0).toUpperCase() + Rating.type.slice(1) //CAPITALIZE FIRST LETTER
+}
+
+function displayRatingsString(Rating){ //CAP "R" for Rating
+	var ratingsString = " " //Empty string
+	Rating.forEach(Rating => {
+			const ratingsSource = Rating.image.match(/^([a-z]+):\/\//i)?.[1]; //has to be in the loop
+
+		ratingsString += ratingsSource + ": " + Rating.value + " " +
+		 "(" + capitolFirstLetter(Rating) + ")" +", "
+	});
+
+	return underline(ratingsString) 
+}
+
+function displayGenresString(genre){
+	var genresString = " " //Empty string
+	genre.forEach(Genre => {
+
+		genresString += Genre.count + " " + Genre.tag + " "
+	});
+
+	return italic(genresString) 
+
+	}
 
 module.exports = {
 	sendChannelNewContent
